68eaddfd872e7b333bca8da37036c05c55cba7b3
Laurentiu Ciobanu <ciobanulaur@gmail.com>
2022-10-14 15:01:13 +0000

    format

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -4,8 +4,10 @@ import { add } from "./add";
 export default function Hello() {
     const sum = add!(1, 3, 6);
 
-    return <div>
-        <h1>Hello world!</h1>
-        <h2>sum is {sum}</h2>
-    </div>;
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <h2>sum is {sum}</h2>
+        </div>
+    );
 }
